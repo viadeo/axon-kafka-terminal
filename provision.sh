5be85e1c1810ea1fbd856307982f93cdcd3486b9
cmurer <cmurer@viadeoteam.com>
2014-04-12 23:16:48 +0200

    provision java and javac 1.7 in order to start zk and kafka

--- provision.sh
+++ provision.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get -y install openjdk-7-jre 
+apt-get -y install openjdk-7-jdk
+
+echo "export JAVA_HOME=$( dirname $( dirname $( readlink -e $(which javac) ) ) )" >> /home/vagrant/.bashrc
+
 wget http://mirrors.ircam.fr/pub/apache/kafka/0.8.1/kafka_2.8.0-0.8.1.tgz
 tar xzf kafka_2.8.0-0.8.1.tgz
 ./kafka_2.8.0-0.8.1/bin/kafka-server-start.sh -daemon ./kafka_2.8.0-0.8.1/config/server.properties

c13fbad4bb43a427c811c521848df7dfa0e3e33a
cmurer <cedric-murer@wanadoo.fr>
2014-04-18 15:01:27 +0200

    generate new topic each time in order to be more robust

--- src/test/java/com/viadeo/axonframework/eventhandling/terminal/kafka/TopicStatementITest.java
+++ src/test/java/com/viadeo/axonframework/eventhandling/terminal/kafka/TopicStatementITest.java
@@ -3,6 +3,8 @@ package com.viadeo.axonframework.eventhandling.terminal.kafka;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -20,31 +22,31 @@ public class TopicStatementITest {
         // Given
 
         // When
-        topicStatement.create("TOTO");
+        topicStatement.create("A");
 
         // Then
-        assertTrue(topicStatement.getTopics().contains("TOTO"));
+        assertTrue(topicStatement.getTopics().contains("A"));
     }
 
     @Test
     public void prepare_withExistingTopic_isOk() {
         // Given
-        topicStatement.create("TOTO");
+        topicStatement.create("B");
 
         // When
-        topicStatement.create("TOTO");
+        topicStatement.create("B");
 
         // Then
-        assertTrue(topicStatement.getTopics().contains("TOTO"));
+        assertTrue(topicStatement.getTopics().contains("B"));
     }
 
     @Test
-    public void remove_withExistingTopic_isOk() {
+    public void remove_withExistingTopic_isOk() throws InterruptedException {
         // Given
-        topicStatement.create("TOTO");
+        final String topic = UUID.randomUUID().toString();
 
         // When
-        topicStatement.remove("TOTO");
+        topicStatement.remove(topic);
 
         // Then
         assertFalse(topicStatement.getTopics().contains("TOTO"));
@@ -55,10 +57,10 @@ public class TopicStatementITest {
         // Given
 
         // When
-        topicStatement.remove("FB");
+        topicStatement.remove("D");
 
         // Then
-        assertFalse(topicStatement.getTopics().contains("FB"));
+        assertFalse(topicStatement.getTopics().contains("D"));
     }
 
 }
